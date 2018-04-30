e66c2c7bd66b06b620867a19665029a5b60f9c0f
Cassandra Afseth <cafseth2@Illinois.edu>
2018-04-30 18:32:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/caafs/weatherapp/Download.java
@@ -0,0 +1,10 @@
+package com.example.caafs.weatherapp;
+
+import android.os.AsyncTask;
+
+public class Download extends AsyncTask<String,Void,String> {
+    @Override
+    protected String doInBackground(String... URL) {
+        return null;
+    }
+}
