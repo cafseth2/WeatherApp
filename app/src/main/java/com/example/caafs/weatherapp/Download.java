f6b8d0f872d6c14560f98bd6870c19ae4d7bec80
Cassandra Afseth <cafseth2@Illinois.edu>
2018-04-30 19:12:12 -0500

    Download class

--- app/src/main/java/com/example/caafs/weatherapp/Download.java
+++ app/src/main/java/com/example/caafs/weatherapp/Download.java
@@ -2,9 +2,48 @@ package com.example.caafs.weatherapp;
 
 import android.os.AsyncTask;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Download extends AsyncTask<String,Void,String> {
     @Override
     protected String doInBackground(String... URL) {
+        String endResult = "";
+        URL url;
+        HttpURLConnection connection = null;
+        try {
+            url = new URL(URL[0]);
+            connection = (HttpURLConnection) url.openConnection();
+            InputStream x = connection.getInputStream();
+            InputStreamReader read = new InputStreamReader(x);
+            int result = read.read();
+            while (result != -1) {
+                char curr = (char) result;
+                endResult += curr;
+                result = read.read();
+            }
+            return endResult;
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+    void execute(String endResult) {
+        super.onPostExecute(endResult);
+        try {
+            JSONObject object = new JSONObject(endResult);
+            JSONObject weatherData = new JSONObject((object.getString("main")));
+            Double temp = Double.parseDouble(weatherData.getString("temp"));
+            int fTemp = (int) (temp * 1.8 - 459.67);
+            String place = object.getString("name");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/caafs/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/caafs/weatherapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.caafs.weatherapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,6 +24,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Download task = new Download();
+        LocationManager location = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        String provide = location.getBestProvider(new Criteria(), false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location locate = location.getLastKnownLocation(provide);
+        Double latitude = locate.getLatitude();
+        Double longitude = locate.getLongitude();
+        task.execute("http://samples.openweathermap.org/data/2.5/weather?lat=" + String.valueOf(latitude) + "&lon=" + String.valueOf(longitude) + "&appid=04f543a486015bfdb8668245febde59a");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
